4b2361621473ad508066f3349b80a9787a101d12
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-10-03 23:23:03 -0400

    Update homepage function

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,19 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route(path: '/', name: 'home', methods: ['GET'])]
-    public function homepage()
+    public function homepage(StarshipRepository $starshipRepository)
     {
-        $starshipCount = 457;
+        $ships = $starshipRepository->findAll();
 
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $starshipCount = count($ships);
+
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
+            'ships' => $ships,
             'myShip' => $myShip,
         ]);
     }
