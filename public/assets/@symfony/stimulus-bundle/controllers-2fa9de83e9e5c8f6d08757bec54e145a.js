9556253b2177e9d04ade9d3d51490c06467bbd46
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-11-19 22:32:36 -0300

    Containerize the app

---
+++ public/assets/@symfony/stimulus-bundle/controllers-2fa9de83e9e5c8f6d08757bec54e145a.js
@@ -0,0 +1,5 @@
+import controller_0 from "../ux-turbo/turbo_controller.js";
+import controller_1 from "../../controllers/closeable_controller.js";
+export const eagerControllers = {"symfony--ux-turbo--turbo-core": controller_0, "closeable": controller_1};
+export const lazyControllers = {};
+export const isApplicationDebug = false;
